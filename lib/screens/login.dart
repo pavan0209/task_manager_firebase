9d2f4175749c5e9296b65ba90aeb5f259738ecbe
pavan0209 <pavanasonawane@gmail.com>
2025-05-31 12:29:56 +0530

    optimized UI for login screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,9 +1,8 @@
-import 'dart:ui';
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import 'package:task_manager_firebase/screens/mixin/index.dart';
-import 'package:task_manager_firebase/screens/widgets/button.dart';
 import 'package:task_manager_firebase/screens/widgets/index.dart';
 import 'package:task_manager_firebase/service/index.dart';
 import 'package:task_manager_firebase/styles/index.dart';
@@ -25,94 +24,98 @@ class _LoginPageState extends BaseState<LoginPage> with BasicPage {
 
   @override
   Widget body(BuildContext context) {
-    return Container(
-      height: double.infinity,
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Color.fromRGBO(255, 223, 175, 1), Color.fromRGBO(184, 222, 255, 1)],
-          begin: AlignmentDirectional.topStart,
-          end: AlignmentDirectional.bottomEnd,
+    final router = AutoRouter.of(context);
+    return SingleChildScrollView(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(255, 215, 155, 1),
+              Color.fromRGBO(184, 222, 255, 1),
+            ],
+            begin: AlignmentDirectional.topStart,
+            end: AlignmentDirectional.bottomEnd,
+          ),
         ),
-      ),
-      child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Center(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.5),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              padding: const EdgeInsets.all(20),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      AppImageAssets.appLogo,
-                      height: 80,
-                    ),
-                    const SizedBox(height: AppSpacing.standardSpacing),
-                    Text('Login', style: AppStyles.blackTitleLargeTextStyle()),
-                    const SizedBox(height: AppSpacing.standardSpacing),
-                    Text('Enter your email and password to log in', style: AppStyles.greyBodySmallTextStyle()),
-                    const SizedBox(height: AppSpacing.extraSpacing),
-                    AppTextField(
-                      controller: _emailController,
-                      filled: true,
-                      fillColor: AppColors.white,
-                      hintText: 'email',
-                      hintStyle: AppStyles.greyBodySmallTextStyle(),
-                      prefixIcon: Icon(Icons.email_outlined, color: AppColors.blue),
-                    ),
-                    const SizedBox(height: AppSpacing.largeSpacing),
-                    AppTextField(
-                      controller: _passwordController,
-                      filled: true,
-                      fillColor: AppColors.white,
-                      obscureText: isObscure,
-                      obscuringCharacter: '*',
-                      suffixIcon: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            isObscure = !isObscure;
-                          });
-                        },
-                        
-                        child: Icon(
-                          isObscure ? Icons.visibility_outlined : Icons.visibility_off_outlined,
-                          color: AppColors.grey,
-                        ),
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.5),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: const EdgeInsets.all(20),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(AppImageAssets.appLogo, height: 80),
+                  const SizedBox(height: AppSpacing.standardSpacing),
+                  Text('Login', style: AppStyles.blackTitleLargeTextStyle()),
+                  const SizedBox(height: AppSpacing.standardSpacing),
+                  Text(
+                    'Enter your email and password to log in',
+                    style: AppStyles.greyBodySmallTextStyle(),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: AppSpacing.extraSpacing),
+      
+                  // Email Field
+                  AppTextField(
+                    controller: _emailController,
+                    filled: true,
+                    fillColor: AppColors.white,
+                    hintText: 'email',
+                    hintStyle: AppStyles.greyBodySmallTextStyle(),
+                    prefixIcon: Icon(Icons.email_outlined, color: AppColors.blue),
+                  ),
+                  const SizedBox(height: AppSpacing.largeSpacing),
+      
+                  // Password Field
+                  AppTextField(
+                    controller: _passwordController,
+                    filled: true,
+                    fillColor: AppColors.white,
+                    obscureText: isObscure,
+                    obscuringCharacter: '*',
+                    hintText: 'password',
+                    hintStyle: AppStyles.greyBodySmallTextStyle(),
+                    prefixIcon: Icon(Icons.lock_outline, color: AppColors.blue),
+                    suffixIcon: GestureDetector(
+                      onTap: () => setState(() => isObscure = !isObscure),
+                      child: Icon(
+                        isObscure ? Icons.visibility_outlined : Icons.visibility_off_outlined,
+                        color: AppColors.grey,
                       ),
-                      hintText: 'password',
-                      hintStyle: AppStyles.greyBodySmallTextStyle(),
-                      prefixIcon: Icon(Icons.lock_outline, color: AppColors.blue),
-
                     ),
-                    const SizedBox(height: AppSpacing.largeSpacing),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
+                  ),
+                  const SizedBox(height: AppSpacing.largeSpacing),
+      
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text('Forgot Password ?', style: AppStyles.blueBodySmallTextStyle()),
+                  ),
+                  const SizedBox(height: AppSpacing.largeSpacing),
+      
+                  AppElevatedButton(onPressed: () {}, label: 'Log In'),
+                  const SizedBox(height: AppSpacing.largeSpacing),
+      
+                  RichText(
+                    text: TextSpan(
                       children: [
-                        Text('Forgot Password ?', style: AppStyles.blueBodySmallTextStyle()),
+                        TextSpan(text: 'Don\'t have an account? ', style: AppStyles.greyBodySmallTextStyle()),
+                        TextSpan(
+                          text: 'Sign Up',
+                          style: AppStyles.blueBodySmallTextStyle(),
+                          recognizer: TapGestureRecognizer()..onTap = () => router.replaceNamed('/register'),
+                        ),
                       ],
                     ),
-                    const SizedBox(height: AppSpacing.largeSpacing),
-                    AppElevatedButton(onPressed: () {}, label: 'Log In'),
-                    const SizedBox(height: AppSpacing.largeSpacing),
-                    RichText(
-                      text: TextSpan(
-                        children: [
-                          TextSpan(text: 'Don\'t have an account? ', style: AppStyles.greyBodySmallTextStyle()),
-                          TextSpan(text: 'Sign Up', style: AppStyles.blueBodySmallTextStyle())
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: AppSpacing.standardSpacing),
-                  ],
-                ),
+                  ),
+                  const SizedBox(height: AppSpacing.standardSpacing),
+                ],
               ),
             ),
           ),
